18edb217cc2732dc23c55970d5fcd3df72c858a7
Jyothika45 <jyothikakt55@gmail.com>
2026-07-15 11:59:34 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AddMedicine from "./components/AddMedicine";
+import ViewMedicine from "./components/ViewMedicine";
+import AddSale from "./components/AddSale";
+import ViewSale from "./components/ViewSale";
+import AddSupplier from "./components/AddSupplier";
+import ViewSupplier from "./components/ViewSupplier";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-   </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-medicine" element={<AddMedicine />} />
+        <Route path="/view-medicine" element={<ViewMedicine />} />
+        <Route path="/add-sale" element={<AddSale />} />
+        <Route path="/view-sale" element={<ViewSale />} />
+        <Route path="/add-supplier" element={<AddSupplier />} />
+        <Route path="/view-supplier" element={<ViewSupplier />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
